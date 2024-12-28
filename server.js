fa433264b3c1f13302bf3881f9311748f91df5d4
Dev-KUN-F <k3614900@gmail.com>
2024-12-28 14:12:28 +0900

    서버측에 cors 설정 추가
    - server.js 에 app.use 를 사용하여 cors 설정을 사용
    
    - 웹과 통신이 가능하게 함

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const youtubeRouter = require("./routes/youtube"); // youtube.js 파일 가져오기
+const cors = require("cors"); //cors 설정 추가
 
 const app = express();
 app.use(express.json()); // JSON 파싱
@@ -7,7 +8,9 @@ app.use(express.urlencoded({ extended: true })); // URL-encoded 데이터 파싱
 
 app.use("/youtube", youtubeRouter); // '/youtube' 경로와 youtube.js 연결
 
-const PORT = 5000;
+app.use(cors()); //cors 사용으로 웹과 통신
+
+const PORT = 5000; // 포트는 5000 번 포트 사용
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
